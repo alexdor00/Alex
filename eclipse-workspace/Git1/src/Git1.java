05e6d4006f025da78dabb7c0f4b18535e1a6b5c9
Alex.git <alexandrudorobatt@gmail.com>
2023-05-19 12:45:14 +0200

    version inicial

---
+++ eclipse-workspace/Git1/src/Git1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author alexa
+ *
+ */
+public class Git1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
